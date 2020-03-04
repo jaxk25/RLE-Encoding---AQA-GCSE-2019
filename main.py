cc1f3b9ce099e52972e5d90da8255bb35c7d26d1
jaxk25 <jack@jaxk.ga>
2020-03-04 20:10:56 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 # Programming Project Task - Jack Greenacre
+# ~ Python 2.7 ~
 
 def menu():
     """
@@ -9,18 +10,44 @@ def menu():
     menuOption = str(menuOption)
     menuOption = menuOption.lower()
     if menuOption == "1" or menuOption == "1. enter rle" or menuOption == "enter rle":
-        print("DEBUG RLE")
+        rle()
     elif menuOption == "2" or menuOption == "2. display ascii art" or menuOption == "display ascii art":
-        print("DEBUG DISPLAY ASCII")
+        displayArt()
     elif menuOption == "3" or menuOption == "3. convert to ascii art" or menuOption == "convert to ascii art":
-        print("DEBUG CONVERT ASCII ART")
+        convertArt()
     elif menuOption == "4" or menuOption == "4. convert to rle" or menuOption == "convert to rle":
-        print("DEBUG CONVERT RLE")
+        convertRle()
     elif menuOption == "5" or menuOption == "5. quit" or menuOption == "quit" or menuOption == "exit":
-        print("DEBUG EXIT")
+        print("Thank you for using!")
+        quit()
     else:
-        print("DEBUG ERROR")
-        
+        print("Error: Input could not be recognised.\nPlease try a recognised option - e.g. `1` or `Enter RLE`\n")
+        menu()
+
+
+def rle():
+    """
+    ENTER COMPRESSED DATA TO UNCOMPRESS
+    """
+    print("DEBUG: OPTION 1 COMING SOON")
+
+def displayArt():
+    """
+    ENTER DATA AND DISPLAY AS ASCII ART
+    """
+    print("DEBUG: OPTION 2 COMING SOON")
+
+def convertArt():
+    """
+    ENTER COMPRESSED DATA AND CONVERT TO ASCII ART
+    """
+    print("DEBUG: OPTION 3 COMING SOON")
+
+def convertRle():
+    """
+    ENTER DATA TO COMPRESS TO RLE
+    """
+    print("DEBUG: OPTION 4 COMING SOON")
 
 #MAIN
 print("Programming Project Task - Jack Greenacre\n")
