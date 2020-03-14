992407e409f0788d1db536879615e6123b2d8a90
jaxk25 <jack@jaxk.ga>
2020-03-14 11:55:42 +0000

    Update files
    Finished rleEncode
    Remove test files

--- main.py
+++ main.py
@@ -142,6 +142,30 @@ def convertRleMain():
     print("\nDEBUG: OPTION 4 COMING SOON\n")
     menu()
 
+def rleEncode(stringToEncode):
+    count = 0
+    chatacter = ''
+    previous_char = stringToEncode[0]
+    result = ''
+    length = len(stringToEncode) 
+    i = 0
+    while (i != length ):
+        chatacter = stringToEncode[i]
+        if previous_char == chatacter :
+            count = count + 1
+        else :
+            countToReturn = str(count)
+            if int(countToReturn) < 10:
+                countToReturn = "0" +str(countToReturn)
+            result = result + str(countToReturn) + previous_char
+            count = 1
+        previous_char = chatacter
+        i = i + 1
+    countToReturn = str(count)
+    if int(countToReturn) < 10:
+        countToReturn = "0" +str(countToReturn)
+    return result + str(countToReturn) + str(previous_char)
+
 #MAIN
 print("Programming Project Task - Jack Greenacre\n")
 try:
