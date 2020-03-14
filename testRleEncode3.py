2d9fe5671f234b52cac9d86ca4d9201897392d5b
jaxk25 <jack@jaxk.ga>
2020-03-14 11:41:19 +0000

    Update testRleEncode3.py
    Streamline sourcecode

--- testRleEncode3.py
+++ testRleEncode3.py
@@ -4,11 +4,9 @@ def encode(message):
     previous_char = message[0]
     result = ''
     length = len(message) 
-
     i = 0
     while (i != length ):
         chatacter = message[i]
-
         if previous_char == chatacter :
             count = count + 1
         else :
@@ -17,14 +15,11 @@ def encode(message):
                 countToReturn = "0" +str(countToReturn)
             result = result + str(countToReturn) + previous_char
             count = 1
-        #print(str(i) + str(chatacter) + str(previous_char) + "          " + str(result))
         previous_char = chatacter
         i = i + 1
-    
     countToReturn = str(count)
     if int(countToReturn) < 10:
         countToReturn = "0" +str(countToReturn)
     return result + str(countToReturn) + str(previous_char)
-
 encoded_message=encode("AAAAAAAAAABBBBCCCCCCCCAB/__")
 print(encoded_message)
